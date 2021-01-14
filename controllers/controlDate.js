34334f172c1cb202382053e4942282591f0e3433
Adam Chu <adamchu2001@gmail.com>
2021-01-14 19:45:53 +0000

    commit

--- controllers/controlDate.js
+++ controllers/controlDate.js
@@ -13,7 +13,7 @@ exports.create = async (req, res) => {
 
 exports.list = async (req, res) => {
     try {
-        console.log(req.params.id);
+        //console.log(req.params.id);
         const Dates = await controlDate.find({_id: req.params.id});
         //console.log(Dates);
         res.render("controlDate", { Dates: Dates },)
@@ -26,7 +26,7 @@ exports.list = async (req, res) => {
 
 exports.update = async (req, res) => {
     try {
-        const date = await controlDate.findOneAndUpdate(req.params.id, { Date: req.params.date, Abap: parseFloat(req.params.abap), Ada: parseFloat(req.params.ada), C_Sharp: parseFloat(req.params.c_sharp), CorCPlusPlus: parseFloat(req.params.corcplusplus), Cobol: parseFloat(req.params.cobol), Delphi: parseFloat(req.params.delphi), Go: parseFloat(req.params.go), Groovy: parseFloat(req.params.groovy), Haskell: parseFloat(req.params.haskell), Java: parseFloat(req.params.java), Javascript: parseFloat(req.params.javascript), Julia: parseFloat(req.params.julia), Kotlin: parseFloat(req.params.kotlin), Lua: parseFloat(req.params.lua), Matlab: parseFloat(req.params.matlab), Objective_C: parseFloat(req.params.obective_c), Perl: parseFloat(req.params.perl), PHP: parseFloat(req.params.php), Python: parseFloat(req.params.python), R: parseFloat(req.params.r), Ruby: parseFloat(req.params.ruby), Rust: parseFloat(req.params.rust), Scala: parseFloat(req.params.scala), Swift: parseFloat(req.params.swift), TypeScript: parseFloat(req.params.typescript), VBA: parseFloat(req.params.vba), Visual_Basic: parseFloat(req.params.visual_basic), Dart: parseFloat(req.params.dart) }, { new: true });
+        const date = await controlDate.findByIdAndUpdate(req.params.id, { Date: req.params.date, Abap: parseFloat(req.params.abap), Ada: parseFloat(req.params.ada), C_Sharp: parseFloat(req.params.c_sharp), CorCPlusPlus: parseFloat(req.params.corcplusplus), Cobol: parseFloat(req.params.cobol), Delphi: parseFloat(req.params.delphi), Go: parseFloat(req.params.go), Groovy: parseFloat(req.params.groovy), Haskell: parseFloat(req.params.haskell), Java: parseFloat(req.params.java), Javascript: parseFloat(req.params.javascript), Julia: parseFloat(req.params.julia), Kotlin: parseFloat(req.params.kotlin), Lua: parseFloat(req.params.lua), Matlab: parseFloat(req.params.matlab), Objective_C: parseFloat(req.params.obective_c), Perl: parseFloat(req.params.perl), PHP: parseFloat(req.params.php), Python: parseFloat(req.params.python), R: parseFloat(req.params.r), Ruby: parseFloat(req.params.ruby), Rust: parseFloat(req.params.rust), Scala: parseFloat(req.params.scala), Swift: parseFloat(req.params.swift), TypeScript: parseFloat(req.params.typescript), VBA: parseFloat(req.params.vba), Visual_Basic: parseFloat(req.params.visual_basic), Dart: parseFloat(req.params.dart) }, { new: true });
         //console.log(date)
         res.redirect("/dates/?message=date has been updated");
     }
